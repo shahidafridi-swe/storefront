35024d617ab17866b76739895e7e313cef863239
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-04 19:27:19 +0600

    13 hello world (render)

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('hello world')
-
+    return render(request, 'hello.html')
