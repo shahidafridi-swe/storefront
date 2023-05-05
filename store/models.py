dfabb0855e29e5bab943428aed9a905161772085
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-05 19:22:51 +0600

    24 ManyToMany Relationship

--- store/models.py
+++ store/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Promotion(models.Model):
+    decription = models.CharField(max_length=255)
+    discount = models.FloatField()
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -12,7 +15,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-
+    promotions = models.ManyToManyField(Promotion)
     BRONZE = "B"
     SILVER = "S"
     GOLD = "G"
