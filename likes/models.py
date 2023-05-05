894fe17f34e5ee9aeac954a2ba4824394d276e91
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-05 22:58:42 +0600

    26 Create a new app 'likes' and create a model class with GenericForeignKey

--- likes/models.py
+++ likes/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
 
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    
-    #product = models.ForeignKey(Product)
-    #type (product, article, video)
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    #ID 
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
