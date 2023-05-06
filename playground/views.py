67288cdc41ba52b424cb40cb81dd2d7dfbf7d39c
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-06 15:17:21 +0600

    44 Product.objects.filter(         Q(inventory__lt=10) | Q(unit_price__lt=20))

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Q
 from store.models import Product
+
 
 def say_hello(request):
 
     # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20) #same of above
+    # queryset = Product.objects.filter(inventory__lt=10).filter(
+    #     unit_price__lt=20)  # same of above
+    
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10) | Q(unit_price__lt=20)) 
 
     context = {
         'products': queryset,
