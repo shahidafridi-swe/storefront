95277c1e99881ec5fd30c687a130eb836a688957
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-06 15:24:31 +0600

    45 Product.objects.filter(inventory=F('unit_price'))

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
@@ -9,8 +9,7 @@ def say_hello(request):
     # queryset = Product.objects.filter(inventory__lt=10).filter(
     #     unit_price__lt=20)  # same of above
 
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) & ~Q(unit_price__lt=20)) 
+    queryset = Product.objects.filter(inventory=F('unit_price')) 
 
     context = {
         'products': queryset,
