732051db2833f012fd52129c10794eb3674acbe3
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-06 15:12:42 +0600

    43 Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,8 @@ from store.models import Product
 
 def say_hello(request):
 
-    queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20) #same of above
 
     context = {
         'products': queryset,
