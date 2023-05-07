13365289f47094c1f8a81e49a69404821f57319c
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-07 18:22:38 +0600

    71 Providing links to othe page

--- store/admin.py
+++ store/admin.py
@@ -17,7 +17,7 @@ class CollectionAdmin(admin.ModelAdmin):
             reverse('admin:store_product_changelist')
             + '?'
             + urlencode({
-                'collection_id':str(collection.id)
+                'collection_id': str(collection.id)
             })
         )
         return format_html('<a href="{}"> {} </a>', url, collection.products_count)
@@ -49,10 +49,27 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+        # reverse('admin:app_model_page')
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer_id': str(customer.id)
+            })
+        )
+        return format_html('<a href="{}"> {} </a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 
 @admin.register(models.Order)
