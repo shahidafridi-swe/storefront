c3f6bd9de22693638fd17a2d44d01c59c1a0e3f7
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-07 15:05:23 +0600

    67 Customize the Product List in administration

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Collection)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
 
-admin.site.register(models.Product)
+admin.site.register(models.Collection)
