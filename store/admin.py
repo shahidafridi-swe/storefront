eef2af0488585a5247ca6259fa210159be54cae7
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-07 19:04:51 +0600

    73 Adding Filtering to the List Page

--- store/admin.py
+++ store/admin.py
@@ -33,6 +33,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update']
     list_per_page = 10
 
     list_select_related = ['collection']
