22baadf3c38db09557a187e92332b675d388bb51
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-07 15:10:58 +0600

    67 Customize the Customer List in administration

--- store/admin.py
+++ store/admin.py
@@ -7,4 +7,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_per_page = 10
 
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 admin.site.register(models.Collection)
