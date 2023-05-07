06e83f9ca94f930dd3894776235bbb55b764ee6e
afridi-cse <shahidafridi.cse@gmail.com>
2023-05-07 17:36:17 +0600

    71 Providing links to othe page

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 from . import models
 
@@ -7,36 +9,43 @@ from . import models
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
-    
+
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
-    
+        # reverse('admin:app_model_page')
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection_id':str(collection.id)
+            })
+        )
+        return format_html('<a href="{}"> {} </a>', url, collection.products_count)
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             products_count=Count('product')
         )
-    
-
-
-
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
 
     list_select_related = ['collection']
+
     def collection_title(self, product):
         return product.collection.title
 
     @admin.display(ordering='inventory')
-    def inventory_status(self,product):
-        if product.inventory < 10 :
+    def inventory_status(self, product):
+        if product.inventory < 10:
             return 'Low'
         return 'OK'
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -49,7 +58,3 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
-    
-
-
-
